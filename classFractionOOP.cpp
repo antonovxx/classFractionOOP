3e856c95c50fb64232323fd0ac39a521b9beb73f
anton <anton@DESKTOP-I0M0TJ2>
2021-12-01 19:18:20 +0300

    Добавьте файлы проекта.

---
+++ classFractionOOP.cpp
@@ -0,0 +1,178 @@
+﻿#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Fraction
+{
+	int integer;
+	int numerator;
+	int denominator;
+public:
+	int get_integer()const
+	{
+		return integer;
+	}
+	int get_numerator()const
+	{
+		return numerator;
+	}
+	int get_denominator()const
+	{
+		return denominator;
+	}
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1;	//Отфильтровываем 0
+		this->denominator = denominator;
+	}
+	//				Constructors:
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefaultConstruct:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1argConstructor:\t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		set_denominator(denominator);	//Чтобы не обойти фильтрацию данных
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//					Methods:
+	void to_improper()//Переводит дробь в неправильну
+	{
+		numerator += integer * denominator;
+		integer = 0;
+	}
+	void to_proper()//Переводит дробь в правильную
+	{
+		integer += numerator / denominator;
+		numerator %= denominator;
+		//numerator = numerator % denominator;
+	}
+	void reduce()
+	{
+		if (numerator == 0) return;
+
+		int more, less;
+		int rest;
+		if (numerator > denominator)
+		{
+			more = numerator;
+			less = denominator;
+		}
+		else
+		{
+			less = numerator;
+			more = denominator;
+		}
+		do
+		{
+			rest = more % less;
+			more = less;
+			less = rest;
+		} while (rest);
+		int GCD = more; // GCD - greatest common divisor
+		numerator /= GCD;
+		denominator /= GCD;
+	}
+
+	void print()const
+	{
+		if (integer)cout << integer;//Если есть целая часть, выводим ее на экран
+		if (numerator)
+		{
+			if (integer)cout << "(";
+			cout << numerator << "/" << denominator;
+			if (integer)cout << ")";
+		}
+		else if (integer == 0)cout << 0;
+		cout << endl;
+	}
+};
+
+Fraction operator*(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	Fraction result
+	(
+		left.get_numerator() * right.get_numerator(),
+		left.get_denominator() * right.get_denominator()
+	);
+	/*result.set_numerator(left.get_numerator()*right.get_numerator());
+	result.set_denominator(left.get_denominator()*right.get_denominator());*/
+	result.to_proper();
+	return result;
+}
+
+//#define CONSTRUCTORS_CHECK
+
+int main()
+{
+	setlocale(LC_ALL, "");
+#ifdef CONSTRUCTORS_CHECK
+	Fraction A;	//Default constructor
+	A.print();
+	double b = 5;
+	Fraction B = 5;	//Single-argument constructor
+	B.print();
+	//Fraction C(7);	//Single-argument constructor
+	//C.print();
+	//Fraction D{ 8 };//Single-argument constructor
+	//D.print();
+	Fraction C(3, 4);
+	C.print();
+	Fraction D(2, 3, 4);
+	D.print();
+#endif // CONSTRUCTORS_CHECK
+
+	double a = 2.5;
+	double b = 3.4;
+	double c = a * b;
+	cout << c << endl;
+
+	Fraction A(2, 1, 2);
+	Fraction B(3, 2, 5);
+	Fraction C = A * B;
+	C.print();
+	C.reduce();
+	C.print();
+	
+	Fraction D(840, 3600);
+	D.reduce();
+	D.print();
+
+	return 0;
+}
+
